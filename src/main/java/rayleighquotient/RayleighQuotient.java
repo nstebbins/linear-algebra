0d584cecca6f018810b899c77a0f8ab55b2a739c
Noah Stebbins <nstebbins@appnexus.com>
2018-03-15 14:50:33 -0400

    some repo structure

---
+++ src/main/java/rayleighquotient/RayleighQuotient.java
@@ -0,0 +1,15 @@
+package rayleighquotient;
+
+public class RayleighQuotient {
+
+    /**
+     *
+     * @param A matrix A
+     * @param eigenvector eigenvector of A
+     * @return corresponding eigenvalue of the eigenvector
+     */
+    public static double computeRayleighQuotient(double[][] A, double[] eigenvector) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/util/VectorUtil.java
@@ -0,0 +1,19 @@
+package util;
+
+public class VectorUtil {
+
+    /**
+     * @param vector1 first vector
+     * @param vector2 second vector
+     * @return computes the dot product of the two vectors
+     */
+    // TODO: ensure vectors are the same length
+    public static double dotProduct(double[] vector1, double[] vector2) {
+        double dotProduct = 0;
+        for (int i = 0; i < vector1.length; i++) {
+            dotProduct += vector1[i] * vector2[i];
+        }
+        return dotProduct;
+    }
+
+}
