bfd394c9706985bd659b731b2d12ef7f76dec399
Noah Stebbins <nstebbins@appnexus.com>
2018-03-15 15:52:23 -0400

    power method, pt 1

---
+++ src/main/java/constants/LinearAlgebraConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class LinearAlgebraConstants {
+    // power method
+    public static final int POWER_METHOD_NUM_SIMULATIONS = 10;
+
+    // assertions on doubles
+    public static final double DOUBLE_DELTA = 0.01;
+}
--- src/main/java/powermethod/PowerMethod.java
+++ src/main/java/powermethod/PowerMethod.java
@@ -1,4 +1,16 @@
 package powermethod;
 
+import constants.LinearAlgebraConstants;
+import util.MatrixUtil;
+
 public class PowerMethod {
+
+    public static double[] powerMethod(double[][] matrix, double[] vector){
+        // TODO: fill this in (e.g. not normalized)
+        double[] product = vector;
+        for (int i = 0; i < LinearAlgebraConstants.POWER_METHOD_NUM_SIMULATIONS; i++) {
+            product = MatrixUtil.multiplyVectorByMatrix(matrix, product);
+        }
+        return product;
+    }
 }
